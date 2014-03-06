00a19b6909cbb3b55131a54f888db5907547c035
jeffmc <jeffmc@localhost.localdomain>
2014-03-06 09:05:38 -0500

    added chmod after mkdir

--- bin/standby_replace_data.sh
+++ bin/standby_replace_data.sh
@@ -6,6 +6,8 @@ echo "replacing standby data with the master backup...."
 mv $OPENSHIFT_DATA_DIR/.$version/data  $OPENSHIFT_DATA_DIR/.$version/data.orig
 mkdir $OPENSHIFT_DATA_DIR/.$version/data 
 
+chmod 700 $OPENSHIFT_DATA_DIR/.$version/data
+
 pg_basebackup -R  \
 --pgdata=$OPENSHIFT_DATA_DIR/.$version/data \
 --host=$OPENSHIFT_PG_HOST --port=15000 -U $PG_REMOTE_USER
