adb5b58b4a99ca569d6663487ae4b78e925f9ee9
jeffmc <jeffmc@localhost.localdomain>
2014-03-20 18:37:24 -0400

    fix

--- bin/create-tunnel.sh
+++ bin/create-tunnel.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
 
-echo "in create-tunnel with PG_REMOTE_INTERNAL_IP of " $PG_REMOTE_INTERNAL_IP
+echo "create-tunnel called for " $JEFF_NODE_TYPE
 
+if [ "$JEFF_NODE_TYPE" == "master" ]; then
 nohup ssh -o UserKnownHostsFile=~/pg932/known_hosts \
 -i ~/pg932/pg932_rsa_key \
 -N -L \
-$OPENSHIFT_PG_HOST:15000:$PG_REMOTE_INTERNAL_IP:5432 \
-$PG_REMOTE_USER@$PG_REMOTE_HOST &> /dev/null &
+$OPENSHIFT_PG_HOST:15000:$JEFF_STANDBY_IP:5432 \
+$JEFF_STANDBY_USER@$JEFF_STANDBY_DNS &> /dev/null &
+fi
+
+if [ "$JEFF_NODE_TYPE" == "standby" ]; then
+nohup ssh -o UserKnownHostsFile=~/pg932/known_hosts \
+-i ~/pg932/pg932_rsa_key \
+-N -L \
+$OPENSHIFT_PG_HOST:15000:$JEFF_MASTER_IP:5432 \
+$JEFF_MASTER_USER@$JEFF_MASTER_DNS &> /dev/null &
+fi
 
