aa2c1a2907dbb793e54ee5f476041ee4bc5fb3c3
jeffmc <jeffmc@localhost.localdomain>
2014-05-05 16:41:53 -0400

    fix

--- bin/grant.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SQL_FILE=/tmp/grant.sql
-
-echo "alter role " \"$USER\" " with replication" > /tmp/grant.sql
-psql -U $USER template1 < $SQL_FILE
-
--- bin/stop-tunnel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-source $OPENSHIFT_CARTRIDGE_SDK_BASH
-
-pid=`ps -C ssh -o pid --no-headers`
-client_result "stop tunnel using PID $pid  "
-
-kill `ps -C ssh -o pid --no-headers`
-
